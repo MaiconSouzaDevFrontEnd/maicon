44512eef38362dabe19024d65838f45a0a31e0a1
MaiconSouzaDevFrontEnd <devfrontend.maiconsouza@gmail.com>
2026-05-28 05:29:26 -0300

    ajustando layout

--- maicon/src/app/page.js
+++ maicon/src/app/page.js
@@ -9,44 +9,59 @@ export default function Home() {
       </h1>
 
       <section className={styles.galery}>
+        <h2 className={styles.subtitle}>Certificados</h2>
+
         <div className={styles.imageContainer}>
+
           <h3 className={styles.subtitle}>Certificado de conclusão do curso Tecnologia da Informação</h3>
-          <Image
-          src="/certificados/CertTiMicroCampFront.tiff"
-          alt="Description"
-          height={300}
-          width={300}
-        />
-        <Image
-          src="/certificados/CertTiMicroCampBack.tiff"
-          alt="Description"
-          height={300}
-          width={300}
-        />
+          <figure className={styles.figure}>
+            <Image
+              src="/certificados/CertTiMicroCampFront.tiff"
+              alt="Certificado TI MicroCamp frente"
+              height={222}
+              width={300}
+            />
+          
+            <Image
+              src="/certificados/CertTiMicroCampBack.tiff"
+              alt="Certificado TI MicroCamp verso"
+              height={220}
+              width={300}
+            />
+          </figure>
         </div>
+
         <div className={styles.imageContainer}>
+
           <h3 className={styles.subtitle}>Certificado de conclusão do curso de Manutenção de Computadores</h3> 
-          <Image
-          src="/certificados/CertRemington.tiff"
-          alt="Description"
-          height={300}
-          width={300}
-          />
+          <figure className={styles.figure}>
+            <Image
+              src="/certificados/CertRemington.tiff"
+              alt="Certificado Remington"
+              height={212}
+              width={300}
+            />
+          </figure>
         </div>
+
         <div className={styles.imageContainer}>
+
           <h3 className={styles.subtitle}>Certificado de conclusão do curso de Eletrônica Analógica</h3>
-          <Image
-            src="/certificados/CertEletrAnalogSenaiFront.tiff"
-            alt="Description"
-            height={300}
-            width={300}
-          />
-          <Image
-            src="/certificados/CertEletrAnalogSenaiBack.tiff"
-            alt="Description"
-            height={300}
-            width={300}
-          />
+          <figure className={styles.figure}>
+            <Image
+              src="/certificados/CertEletrAnalogSenaiFront.tiff"
+              alt="Certificado Eletrônica Analógica frente"
+              height={426}
+              width={300}
+            />
+          
+            <Image
+              src="/certificados/CertEletrAnalogSenaiBack.tiff"
+              alt="Certificado Eletrônica Analógica verso"
+              height={467}
+              width={300}
+            />
+          </figure>
         </div>
       </section>
     </main>  
